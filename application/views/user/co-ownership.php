5662ab3a4592637858a3ef623d0058484cb8e674
Oluwaseun <seuncrowther@gmail.com>
2023-08-14 19:33:44 +0100

    Update

--- application/views/user/co-ownership.php
+++ application/views/user/co-ownership.php
@@ -36,7 +36,7 @@
                     <p style="font-size:14px">Bought on <?php echo date('M Y', strtotime($value['request_date'])); ?><br><small class="font-weight-lighter">Co ownership</small></p>
                   </div>
                   <p class="card-text"><?php echo $value['property_name']; ?></p>
-                  <p class="card-text font-weight-lighter"><?php echo ($p_ben == 'Self' || $p_ben == 'Free') ? $value['unit_amount'] : $value['no_of_units']; ?> Shares <?php ($p_ben == 'Free')? '(Free)' : ''; ?></p>
+                  <p class="card-text font-weight-lighter"><?php echo ($p_ben == 'Self' || $p_ben == 'Free') ? $value['unit_amount'] : $value['no_of_units']; ?> Shares <?php echo ($p_ben == 'Free')? '(Free)' : ''; ?></p>
                   <div class=" mt-5">
                     <a href="<?php echo base_url(); ?>user/co-ownership-property/<?php echo $value['reqID']; ?>" class="btn tertiary-background px-5">view</a>
                   </div>
