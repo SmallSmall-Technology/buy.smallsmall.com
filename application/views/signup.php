956b1b7a24908029129ea0d1b62da643b6ccb96d
yusuf <bwitlawalyusuf@gmail.com>
2023-08-25 13:34:38 +0100

    Refer and Earn Page created with Nector

--- application/views/signup.php
+++ application/views/signup.php
@@ -177,12 +177,12 @@
 
 <!-- Nector Code -->
 
-<script>var nector_params = { customer_id: $userID }</script>
+<!-- <script>var nector_params = { customer_id: $userID }</script>
 <script async src="https://cdn.nector.io/nector-static/no-cache/reward-widget/mainloader.min.js"
 	data-op="reward"
 	data-api_key="ca91f88601fb02c3c90db6bbd20da779dbb3f5d07827db9df99e290617d7d63e"
 	data-platform="custom_website"
-></script>
+></script> -->
 
 
 
